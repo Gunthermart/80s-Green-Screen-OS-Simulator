25def224bcf3de4fe648d4e07584cc5c2ca1e046
GuntherMart <gunthermart@hotmail.com>
2025-09-08 21:55:21 +0200

    Add files via upload

--- netlify/functions/get-api-key.js
+++ netlify/functions/get-api-key.js
@@ -1,12 +1,12 @@
 // Cette fonction s'exécute sur les serveurs de Netlify.
 // Elle récupère de manière sécurisée la clé API depuis les variables d'environnement.
 exports.handler = async (event, context) => {
-  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const apiKey = process.env.Maps_API_KEY;
 
   if (!apiKey) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "La variable d'environnement GOOGLE_MAPS_API_KEY n'est pas configurée sur le serveur." }),
+      body: JSON.stringify({ error: "La variable d'environnement Maps_API_KEY n'est pas configurée sur le serveur." }),
     };
   }
 
