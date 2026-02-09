7890eb87fb89d96447a3bd44998ab8d9fc092c4c
GuntherMart <gunthermart@hotmail.com>
2026-02-09 18:25:40 +0100

    Create manifest.js

---
+++ public/AgenData/manifest.js
@@ -0,0 +1,39 @@
+{
+  "name": "AgenData v8.9.12",
+  "short_name": "AgenData",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#0f172a",
+  "theme_color": "#0f172a",
+  "orientation": "portrait-primary",
+  "icons": [
+    {
+      "src": "screenshot/icons-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "screenshot/icons-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ],
+  "screenshots": [
+    {
+      "src": "screenshot/screenshot-mobile.jpg",
+      "sizes": "1170x2532",
+      "type": "image/jpeg",
+      "form_factor": "narrow",
+      "label": "Interface Mobile - Flux Rapide"
+    },
+    {
+      "src": "screenshot/screenshot-desktop.jpg",
+      "sizes": "1920x1080",
+      "type": "image/jpeg",
+      "form_factor": "wide",
+      "label": "Interface Desktop - Matrice Tactique"
+    }
+  ]
+}
