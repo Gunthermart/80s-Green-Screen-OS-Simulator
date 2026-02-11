36804e1b7ae6ba6b8ac74576422bede76b6e5b4d
GuntherMart <gunthermart@hotmail.com>
2026-02-11 10:11:00 +0100

    Update manifest.js

--- public/AgenData/manifest.js
+++ public/AgenData/manifest.js
@@ -1,38 +1,39 @@
 {
-    "name": "AgenData v9.1",
-    "short_name": "AgenData",
-    "description": "Système de Gestion Cognitive. Local. Sécurisé.",
-    "start_url": "./index.html",
-    "display": "standalone",
-    "background_color": "#0f172a",
-    "theme_color": "#0f172a",
-    "orientation": "portrait-primary",
-    "icons": [
-        {
-            "src": "screenshot/icons-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any maskable"
-        },
-        {
-            "src": "screenshot/icons-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any maskable"
-        }
-    ],
-    "screenshots": [
-        {
-            "src": "screenshot/AgenData-mobile.png",
-            "sizes": "390x844",
-            "type": "image/png",
-            "form_factor": "narrow"
-        },
-        {
-            "src": "screenshot/AgenData-desktop.png",
-            "sizes": "1920x1080",
-            "type": "image/png",
-            "form_factor": "wide"
-        }
-    ]
+  "name": "AgenData v9.21",
+  "short_name": "AgenData",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#0f172a",
+  "theme_color": "#0f172a",
+  "orientation": "any",
+  "icons": [
+    {
+      "src": "icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ],
+  "screenshots": [
+    {
+      "src": "screenshoot-mobile.png",
+      "sizes": "1170x2532",
+      "type": "image/png",
+      "form_factor": "narrow",
+      "label": "Interface Mobile Tactique"
+    },
+    {
+      "src": "screenshoot-desktop.png",
+      "sizes": "1920x1080",
+      "type": "image/png",
+      "form_factor": "wide",
+      "label": "Bureau Opérationnel"
+    }
+  ]
 }
