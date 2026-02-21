5838db4b979a1273981e7fdf6ef185f68ccbf981
GuntherMart <gunthermart@hotmail.com>
2026-02-21 23:55:27 +0100

    Update worker.js

--- public/chess/worker.js
+++ public/chess/worker.js
@@ -1,1 +1,23 @@
 
+// Résolution du chemin pour le fichier WASM (si requis par la compilation du CDN)
+var Module = {
+    locateFile: function(path) {
+        return 'https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.0/' + path;
+    }
+};
+
+// Importation du script Stockfish
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.0/stockfish.js');
+
+// Selon la façon dont le script a été packagé, il peut nécessiter une instanciation manuelle
+if (typeof Stockfish === 'function') {
+    var engine = Stockfish();
+    self.onmessage = function(e) { engine.postMessage(e.data); };
+    engine.onmessage = function(msg) { self.postMessage(msg); };
+} else if (typeof STOCKFISH === 'function') {
+    var engine = STOCKFISH();
+    self.onmessage = function(e) { engine.postMessage(e.data); };
+    engine.onmessage = function(msg) { self.postMessage(msg); };
+}
+// Si aucune de ces fonctions n'est trouvée, Emscripten a déjà automatiquement
+// lié le moteur aux événements self.onmessage et self.postMessage du Worker.
